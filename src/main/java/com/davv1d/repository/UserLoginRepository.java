b2a29eda1ce467abb2ea7f5da65be3d7dcfc22b7
davv1d <kowalskidawid63@gmail.com>
2019-11-10 19:52:33 +0100

    add test user login repository

--- src/main/java/com/davv1d/repository/UserLoginRepository.java
+++ src/main/java/com/davv1d/repository/UserLoginRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface UserLoginRepository extends CrudRepository<UserLogin, Long> {
     @Override
     UserLogin save(UserLogin userLogin);
+
+    List<UserLogin> findByUsername(String username);
 }
---
+++ src/test/java/com/davv1d/repository/UserLoginRepositoryTestSuite.java
@@ -0,0 +1,57 @@
+package com.davv1d.repository;
+
+import com.davv1d.domain.user.login.UserLogin;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserLoginRepositoryTestSuite {
+
+    @Autowired
+    private UserLoginRepository userLoginRepository;
+
+    @Test
+    public void shouldSave() {
+        //Given
+        LocalDateTime loginDate = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        UserLogin userLogin = new UserLogin("test username", loginDate);
+        //When
+        userLoginRepository.save(userLogin);
+        //Then
+        Optional<UserLogin> optionalUserLogin = userLoginRepository.findById(userLogin.getId());
+        assertTrue(optionalUserLogin.isPresent());
+        assertEquals("test username", optionalUserLogin.get().getUsername());
+        assertEquals(loginDate, optionalUserLogin.get().getLoginDate());
+        //Clean up
+        userLoginRepository.deleteById(userLogin.getId());
+    }
+
+    @Test
+    public void shouldFindByUsername() {
+        //Given
+        LocalDateTime loginDate = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        UserLogin userLogin1 = new UserLogin("test username", loginDate);
+        UserLogin userLogin2 = new UserLogin("test username", loginDate.plusDays(2));
+        userLoginRepository.save(userLogin1);
+        userLoginRepository.save(userLogin2);
+        //When
+        List<UserLogin> userLogins = userLoginRepository.findByUsername("test username");
+        //Then
+        assertEquals(2, userLogins.size());
+        //Clean up
+        for (UserLogin userLogin: userLogins) {
+            userLoginRepository.deleteById(userLogin.getId());
+        }
+    }
+}
